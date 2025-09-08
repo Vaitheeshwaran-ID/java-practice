05a9cecbfa1586a00fb4140e692911b68b986d78
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2025-09-08 23:59:59 +0530

    basic.java

---
+++ basic.java
@@ -0,0 +1,7 @@
+Java 
+  - High level programming language
+  - object oriented programming language
+  - general purpose programming language
+  - platform independent langauage
+  - by sun-microsystems in 1995 now it is Oracle
+  - robust and multi threading language
