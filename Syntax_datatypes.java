c4e1a9b5bc9606925a68bb642fcb687a45d28257
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-02 06:09:07 +0530

    Create Syntax_datatypes.java with data type examples
    Added a Java class demonstrating primitive and non-primitive data types.

---
+++ Syntax_datatypes.java
@@ -0,0 +1,22 @@
+public class Syntax_datatypes{
+  public static void main (String []args){
+    //primitive data types
+    byte b = 10;
+    short s = 100;
+    int i = 1000;
+    long l=1000000L;
+
+    float f = 10.5f;
+    char c = 'A';
+
+    boolean isPrime=true;
+
+    //Non primitive dataTypes
+    String str = "java";
+    int [] arr = {1,2,3};
+    Classname obj =new classname();
+    
+
+    
+}
+  
