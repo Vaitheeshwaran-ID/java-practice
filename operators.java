0e79fdf63a850f3299af101ea261a02d8b7b0a05
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-04 21:11:08 +0530

    Implement basic arithmetic and logical operations

---
+++ operators.java
@@ -0,0 +1,57 @@
+public class operators{
+  public static void main (String[]args){
+  int a = 10;
+    int b = 3;
+
+    int sum= a+ b;
+    int difference= a-b;
+    int product=a*b;
+    int division =a/b;
+    int modulo=a%b;
+  
+int x=5;
+    x+=2;
+    x*=3;
+
+    boolean isGreater = a>b;
+    boolean isEqual = a == b;
+
+    boolean logiacalResult = (a>b)&&(b>0);
+
+int negative = -a;
+    boolean notTrue = !true;
+
+    int preinc = ++a;
+    int postDec = b--;
+
+    int max = (a>b) ? a:b;
+
+    System.out.println("sum: " + sum/n );
+    System.out.println("difference:" + difference);
+    System.out.println("product" +product );
+    System.out.println("division" +division );
+System.out.println("modulo" +modulo );
+
+        System.out.println("Assignment result x:" + X );
+
+        System.out.println("Is a greater than b?" + isGreator );
+
+        System.out.println("Is a eaual to b" + isEqual );
+
+        System.out.println("Logical And Result" + logicalresult );
+
+        System.out.println("Unary negative of a:" + negative );
+
+        System.out.println("Not true" + notTrue );
+
+        System.out.println("Pre Increment a:" + preinc );
+
+        System.out.println("post Decrement" + postDec );
+
+        System.out.println("Final value of b" + b );
+
+        System.out.println("Maximum Value :" + max );
+
+    
+  }
+}
