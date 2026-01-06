b9b1344feae422114236d62cb8b70705a56c8ac6
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-06 22:57:35 +0530

    Rename class from c1 to palindrome

--- palindrome.java
+++ palindrome.java
@@ -1,7 +1,7 @@
 package codingtraining;
 import java.util.*;
 
-public class c1 {
+public class palindrome {
 	public static void main(String[]args) {
 	Scanner sc = new Scanner(System.in);
 int og =sc.nextInt();
