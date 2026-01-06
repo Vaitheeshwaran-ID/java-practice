d43ca6bf0958cd02ae0f5c6ecab24d7cf144e28d
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-06 22:56:21 +0530

    Implement palindrome checker in palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+package codingtraining;
+import java.util.*;
+
+public class c1 {
+	public static void main(String[]args) {
+	Scanner sc = new Scanner(System.in);
+int og =sc.nextInt();
+int ogdup=og;
+int rev=0;
+while(og > 0) {
+	int dig=og%10;
+	rev=rev*10 + dig;
+	og/=10;
+	
+}
+
+if(ogdup==rev) {
+	System.out.println("It is a Palindrome");
+}else {
+	System.out.println("It is not a palindrome");
+}
+	
+	}
+
+}
