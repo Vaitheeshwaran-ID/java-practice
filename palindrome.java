4ecfc86ff37bb63448268e0177c312143e2e429a
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-07 21:12:45 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,25 +1,23 @@
-package codingtraining;
 import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int og =sc.nextInt();
+        
+        int ogdup=og;
+        int num=og;
+        int rev=0;
+        while(og>0){
+            int dig=og%10;
+            rev=rev*10+dig;
+            og/=10;
+        }
+        if(rev==ogdup){
+            System.out.println("yes palindrome");
+        }else{
+            System.out.println("Not a palindrome");
+        
+            }
+        
+    }}
 
-public class palindrome {
-	public static void main(String[]args) {
-	Scanner sc = new Scanner(System.in);
-int og =sc.nextInt();
-int ogdup=og;
-int rev=0;
-while(og > 0) {
-	int dig=og%10;
-	rev=rev*10 + dig;
-	og/=10;
-	
-}
-
-if(ogdup==rev) {
-	System.out.println("It is a Palindrome");
-}else {
-	System.out.println("It is not a palindrome");
-}
-	
-	}
-
-}
