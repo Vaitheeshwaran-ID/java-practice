04215c64be9cc0df147e9eb927ec916bf2e92ac1
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-10 22:45:12 +0530

    Change square pattern size from 3 to 4

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,6 +1,6 @@
 public class SquarePattern {
     public static void main(String[] args) {
-        int n=3;
+        int n=4;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print("*");
