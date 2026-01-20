dfd1871f49a2d1e9ab2eccbcf3b015d5a4e4126d
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-20 21:02:43 +0530

    Create ArcadeGame.java

---
+++ ArcadeGame.java
@@ -0,0 +1,11 @@
+/*Arcade Game
+
+In an arcade game, you get two bonus points a and b. If either a or b is exactly 10, or if their sum is 10, then your total score is 0. Otherwise, your total score is sum of a and b. Return the final score.
+Examples:
+finalScore(10, 5)
+→ 0
+finalScore(4, 6)
+→ 0
+finalScore(3, 4)
+→ 7
+*/
