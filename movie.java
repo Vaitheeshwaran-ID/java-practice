b5dc04d7829523f44dfed47c06ab66129874761c
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-22 23:03:03 +0530

    Update comments in movie.java for clarity

---
+++ movie.java
@@ -0,0 +1,9 @@
+// Movie
+// It's the weekend and you are deciding whether to watch a movie or rest, watch the movie only if it is the weekend and you are not tired; otherwise, rest. Return the appropirate string value.
+// Examples:
+// movie(false, false)
+// → REST
+// movie(true, false)
+// → WATCH
+// movie(false, true)
+// → REST
