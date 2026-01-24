5c83bab102d294e42b9e5e413b7ec245d399f976
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-01-24 23:41:21 +0530

    Fix loop to correctly print multiplication table

--- Print.java
+++ Print.java
@@ -1,7 +1,7 @@
 public class PrintTable{
   public static void main(String[]args){
     int n=9;
-    for(int i=0;i<=10;i++){
+    for(int i=1;i<=10;i++){
       System.out.println(i+"*"+"9"+"="+i*9);
     }
   }
