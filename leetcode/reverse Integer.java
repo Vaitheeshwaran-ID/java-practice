cdbae82ae7e02fdd5dd488f3ffc6da9e444af877
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-03 22:21:15 +0530

    Fix integer overflow checks in reverse method

--- leetcode/reverse Integer.java
+++ leetcode/reverse Integer.java
@@ -3,13 +3,10 @@ class Solution {
         int rev=0;
         while(x!=0){
             int a=x%10;
-
             if(rev > Integer.MAX_VALUE /10 || (rev == Integer.MAX_VALUE /10 && a > 7)){
             return 0;}
-
             if(rev < Integer.MIN_VALUE /10||(rev == Integer.MIN_VALUE /10 && a < -8))
            { return 0;}
-
             rev=rev*10+a;
             x=x/10;
         }
