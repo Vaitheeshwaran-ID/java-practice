a714375111e7f38070c5817651b48cdf0ac1b839
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-05 23:37:57 +0530

    Implement anagram check in anagram.java
    Add anagram checking functionality to compare two strings.

--- anagram.java
+++ anagram.java
@@ -1,1 +1,20 @@
 
+import java.util.Arrays;
+
+public class _040526_2 {
+
+	public static void main(String[] args) {
+	String s="dad";
+	String s1="add";
+	if(s.length()!=s1.length()) {
+		System.out.println("no");
+	}else {
+		char []a=s.toCharArray();
+		char []b=s1.toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		System.out.println(Arrays.equals(a,b)?"Yes":"No");
+			
+		}
+
+	}
