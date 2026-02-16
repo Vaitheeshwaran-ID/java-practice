e77c86a39c0aa1066ba26914f8378d83a5744b55
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-02-16 23:30:51 +0530

    Update valid parentheses.java

--- leetcode/valid parentheses.java
+++ leetcode/valid parentheses.java
@@ -1,1 +1,20 @@
-valid parentheses
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> st= new Stack<>();
+	    for(char ch: s.toCharArray()) {
+		    if(ch == '(') {
+			    st.push(')');
+		    }
+		    else if(ch == '{') {
+			    st.push('}');
+		    }
+		    else if(ch == '[') {
+			    st.push(']');
+		    }
+		    else if(st.isEmpty() || st.pop() !=ch) {
+			    return false;
+		    }
+	    }
+	    return st.isEmpty(); 
+    }
+}
