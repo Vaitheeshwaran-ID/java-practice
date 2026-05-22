e0ade446a16b6648ee8773bc4a06c818bd1d0a13
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-05-22 22:14:32 +0530

    Fix typo in description of Java as a language

--- basic.java
+++ basic.java
@@ -1,7 +1,7 @@
 Java 
   - High level programming language
   - object oriented programming language
-  - general purpose programming language
+  - general purpose programming languages
   - platform independent langauage
   - by sun-microsystems in 1995 now it is Oracle
   - robust and multi threading language
