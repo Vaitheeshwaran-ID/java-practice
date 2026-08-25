090ea336aada453b7be8aaba16f62f6483baa38e
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-08-25 22:27:05 +0530

    Remove unnecessary comment in addfunctions.java
    Removed unnecessary comment about needing more practice.

--- methods/addfunctions.java
+++ methods/addfunctions.java
@@ -6,7 +6,7 @@ public static int sum(int a,int b ) {
 	int sum = a+b;
 	return sum;
 }
-
+//need more practice
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		int a =sc.nextInt();
